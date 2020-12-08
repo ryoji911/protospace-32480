2ff60453c8839bdd43773f2eb8748e12945b19b6
ryoji911 <ryoji407_1109@yahoo.co.jp>
2020-12-08 16:31:19 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,10 @@
+class Prototype < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+end
